c3e12954a47fa1eeed5787d51b147fb64ec089b9
Nima Soufiani <n_soufiani@hotmail.com>
2019-12-22 00:37:41 +0000

    more kata

--- JavascriptKata.js
+++ JavascriptKata.js
@@ -48,121 +48,212 @@ function rot13(str) {
 
 // Another way of doing it
 
-function rot13Aw(str) {
-  var codeA = "A".charCodeAt(0);
-  var codeN = "N".charCodeAt(0);
-  var codeZ = "Z".charCodeAt(0);
-  var newArr = [];
+// function rot13Aw(str) {
+//   var codeA = "A".charCodeAt(0);
+//   var codeN = "N".charCodeAt(0);
+//   var codeZ = "Z".charCodeAt(0);
+//   var newArr = [];
 
-  for (var i = 0; i < str.length; i++) {
-    var code = str.charCodeAt(i);
-    if (code >= codeA && code <= codeZ) {
-      if (code >= codeN) newArr.push(String.fromCharCode(code - 13));
-      else newArr.push(String.fromCharCode(code + 13));
-    } else {
-      newArr.push(str[i]);
-    }
-  }
-  return newArr.join("");
-}
+//   for (var i = 0; i < str.length; i++) {
+//     var code = str.charCodeAt(i);
+//     if (code >= codeA && code <= codeZ) {
+//       if (code >= codeN) newArr.push(String.fromCharCode(code - 13));
+//       else newArr.push(String.fromCharCode(code + 13));
+//     } else {
+//       newArr.push(str[i]);
+//     }
+//   }
+//   return newArr.join("");
+// }
 
-console.log(rot13Aw("HELLO AND WELCOME"));
+// console.log(rot13Aw("HELLO AND WELCOME"));
 
-// New way of doing it
+// // New way of doing it
 
-rot13MyMethod = str => {
-  let charA = "A".charCodeAt(0);
-  let charN = "N".charCodeAt(0);
-  let charZ = "Z".charCodeAt(0);
-  let factor = 13;
-  let newArr = [];
+// rot13MyMethod = str => {
+//   let charA = "A".charCodeAt(0);
+//   let charN = "N".charCodeAt(0);
+//   let charZ = "Z".charCodeAt(0);
+//   let factor = 13;
+//   let newArr = [];
 
-  for (let i = 0; i < str.length; i++) {
-    let code = str.charCodeAt(i);
-    if (code >= charA && code <= charZ) {
-      if (code >= charN) {
-        newArr.push(String.fromCharCode(code - factor));
-      } else {
-        newArr.push(String.fromCharCode(code + factor));
-      }
-    } else {
-      newArr.push(str[i]);
-    }
-  }
-  return newArr.join("");
-};
+//   for (let i = 0; i < str.length; i++) {
+//     let code = str.charCodeAt(i);
+//     if (code >= charA && code <= charZ) {
+//       if (code >= charN) {
+//         newArr.push(String.fromCharCode(code - factor));
+//       } else {
+//         newArr.push(String.fromCharCode(code + factor));
+//       }
+//     } else {
+//       newArr.push(str[i]);
+//     }
+//   }
+//   return newArr.join("");
+// };
 
-console.log(rot13MyMethod("MY HELLO"));
+// console.log(rot13MyMethod("MY HELLO"));
 
-rot13Again = str => {
-  let chatA = "A".charCodeAt(0);
-  let chatN = "N".charCodeAt(0);
-  let chatZ = "Z".charCodeAt(0);
-  let factor = 13;
-  let newArr = [];
+// rot13Again = str => {
+//   let chatA = "A".charCodeAt(0);
+//   let chatN = "N".charCodeAt(0);
+//   let chatZ = "Z".charCodeAt(0);
+//   let factor = 13;
+//   let newArr = [];
 
-  for (let i = 0; i < str.length; i++) {
-    let code = str.charCodeAt(i);
-    if (code > chatA && code < chatZ) {
-      if (code >= chatN) {
-        newArr.push(String.fromCharCode(code - factor));
-      } else {
-        newArr.push(String.fromCharCode(code + factor));
-      }
-    } else {
-      newArr.push(str[i]);
-    }
-  }
-  return newArr.join("");
-};
+//   for (let i = 0; i < str.length; i++) {
+//     let code = str.charCodeAt(i);
+//     if (code > chatA && code < chatZ) {
+//       if (code >= chatN) {
+//         newArr.push(String.fromCharCode(code - factor));
+//       } else {
+//         newArr.push(String.fromCharCode(code + factor));
+//       }
+//     } else {
+//       newArr.push(str[i]);
+//     }
+//   }
+//   return newArr.join("");
+// };
 
-console.log(rot13Again("HELLO MY NAME IS NIMA"));
+// console.log(rot13Again("HELLO MY NAME IS NIMA"));
 
-//rot13 yet again
+// //rot13 yet again
 
-rot13YetAgain = str => {
-  let charA = "A".charCodeAt(0);
-  let charN = "N".charCodeAt(0);
-  let charZ = "Z".charCodeAt(0);
-  let newArr = [];
-  let factor = 13;
+// rot13YetAgain = str => {
+//   let charA = "A".charCodeAt(0);
+//   let charN = "N".charCodeAt(0);
+//   let charZ = "Z".charCodeAt(0);
+//   let newArr = [];
+//   let factor = 13;
 
-  for (let i = 0; i < str.length; i++) {
-    let code = str.charCodeAt(i);
-    if (code >= charA && code <= charZ) {
-      if (code >= charN) {
-        newArr.push(String.fromCharCode(code - factor));
-      } else {
-        newArr.push(String.fromCharCode(code + 13));
-      }
-    } else {
-      newArr.push(str[i]);
-    }
-  }
-  return newArr.join("");
-};
+//   for (let i = 0; i < str.length; i++) {
+//     let code = str.charCodeAt(i);
+//     if (code >= charA && code <= charZ) {
+//       if (code >= charN) {
+//         newArr.push(String.fromCharCode(code - factor));
+//       } else {
+//         newArr.push(String.fromCharCode(code + 13));
+//       }
+//     } else {
+//       newArr.push(str[i]);
+//     }
+//   }
+//   return newArr.join("");
+// };
 
-console.log(rot13YetAgain("OOPS I DID IT AGAIN"));
+// console.log(rot13YetAgain("OOPS I DID IT AGAIN"));
 
 //****** FIZZBUZZ TEST ******/
 //return fizzbuzz if number divided by 3 and 5 has no decimals.
 //return fizz if number divided by 3 has no decimals
 //return buzz if number divided by 5 has no decimals
 
-fizzBuzz = () => {
-  let newArr = [];
-  for (let i = 0; i < 101; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      newArr.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-      newArr.push("Fizz");
-    } else if (i % 5 === 0) {
-      newArr.push("Buzz");
-    } else {
-      newArr.push(i);
+// fizzBuzz = () => {
+//   let newArr = [];
+//   for (let i = 0; i < 101; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       newArr.push("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       newArr.push("Fizz");
+//     } else if (i % 5 === 0) {
+//       newArr.push("Buzz");
+//     } else {
+//       newArr.push(i);
+//     }
+//   }
+//   return newArr;
+// };
+
+//console.log(fizzBuzz());
+
+//******************Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained. */
+
+//example:  "This is an example!" ==> "sihT si na !elpmaxe"
+//          "double  spaces"      ==> "elbuod  secaps"
+
+reverseWords = str => {
+  return str
+    .split(" ")
+    .map(word => {
+      return word
+        .split("")
+        .reverse()
+        .join("");
+    })
+    .join(" ");
+};
+console.log(reverseWords("This is an example"));
+
+//Reverse words on my own
+
+function reverseWordsOMO(str) {
+  return str
+    .split(" ")
+    .map(function(word) {
+      return word
+        .split("")
+        .reverse()
+        .join("");
+    })
+    .join(" ");
+}
+
+console.log(reverseWordsOMO("lets try reversing this"));
+
+/**
+ * The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Note for F#: The input will be of (int list list) which is a List<List>
+
+Example Input
+[[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+ */
+
+membership = list => {
+  let membershipOption = [];
+  list.map(member => {
+    if (member[0] >= 55 && member[1] > 7) {
+      membershipOption.push("Senior");
+    } else if (member[0] >= 55 && member[1] <= 7) {
+      membershipOption.push("Open");
+    } else if (member[0] < 55) {
+      membershipOption.push("Open");
     }
-  }
-  return newArr;
+  });
+  return membershipOption;
 };
 
-console.log(fizzBuzz());
+console.log(
+  membership([
+    [18, 20],
+    [45, 2],
+    [61, 12],
+    [37, 6],
+    [21, 21],
+    [78, 9]
+  ])
+);
+
+//Pro solution
+function openOrSenior(data) {
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? "Senior" : "Open"
+  );
+}
+
+console.log(
+  openOrSenior([
+    [18, 20],
+    [45, 2],
+    [61, 12],
+    [37, 6],
+    [21, 21],
+    [78, 9]
+  ])
+);
